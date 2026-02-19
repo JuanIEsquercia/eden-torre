e57c2121b6d078999654b265a3491bfa399d0c94
Juan Esquercia <jiesquercia@gmail.com>
2026-02-19 09:13:55 -0300

    fix: add className prop to AgencyLogo to fixing build error

--- src/components/public/AgencyLogo.tsx
+++ src/components/public/AgencyLogo.tsx
@@ -5,16 +5,17 @@ import { CldImage } from 'next-cloudinary'
 interface AgencyLogoProps {
     src: string
     alt: string
+    className?: string
 }
 
-export function AgencyLogo({ src, alt }: AgencyLogoProps) {
+export function AgencyLogo({ src, alt, className = '' }: AgencyLogoProps) {
     return (
         <CldImage
             width="160"
             height="80"
             src={src}
             alt={alt}
-            className="object-contain max-h-24 grayscale opacity-70 transition-all duration-300 group-hover:grayscale-0 group-hover:opacity-100"
+            className={`object-contain max-h-24 grayscale opacity-70 transition-all duration-300 group-hover:grayscale-0 group-hover:opacity-100 ${className}`}
             format="auto"
             quality="auto"
         />
